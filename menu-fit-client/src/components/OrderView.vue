e347d8235b0293763179b64a57638c0bcbb9bb19
Shuangbing <he.shuangbing@gmail.com>
2019-10-10 01:16:41 +0900

    add client order view

--- menu-fit-client/src/components/OrderView.vue
+++ menu-fit-client/src/components/OrderView.vue
@@ -1,50 +1,105 @@
 <template>
-  <div class="md-example-child md-example-child-cell-item md-example-child-cell-item-2">
-    <md-field>
-      <md-cell-item title="交通银行(尾号3089)" brief="展示摘要描述" />
-      <md-cell-item title="招商银行(尾号2342)" brief="展示摘要描述">
-        <md-switch v-model="open" slot="right" />
-      </md-cell-item>
-      <md-cell-item title="交通银行(尾号3089)" brief="展示摘要描述" addon="附加文案" arrow />
-      <md-cell-item title="交通银行" brief="展示摘要描述" addon="附加文案" arrow>
-        <span class="holder" slot="left"></span>
-      </md-cell-item>
-      <md-cell-item title="招商银行" brief="展示摘要描述" addon="禁用的项目" disabled arrow>
-        <span class="holder" slot="left"></span>
-      </md-cell-item>
-      <md-cell-item title="Mand Mobile" brief="A mobile UI toolkit" arrow>
-        <p slot="children" style="font-size:0.24rem;color:#858B9C;">
-          面向金融场景的Vue移动端UI组件库，丰富、灵活、实用，快速搭建优质的金融类产品，让复杂的金融场景变简单。基于「合理、好用」设计价值观，从交互操作、视觉抽象、图形可视等角度共同解决问题。
-        </p>
-      </md-cell-item>
-    </md-field>
+  <div>
+    <div class="header">
+      <div class="title">
+        <p>Demoレストラン</p>
+        <md-tag size="large" shape="circle" type="ghost" font-color="#FC7353">A-1</md-tag>
+      </div>
+      <img
+        class="title"
+        width="100"
+        height="100"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxiYsRILC5q59QRcTvXn9tAEDi3VCuXFtc1TLOQh5p-10gBm0J"
+      />
+    </div>
+    <md-tab-bar v-model="current" :items="items" :maxLength="5" />
+    <div class="md-example-child md-example-child-cell-item md-example-child-cell-item-2">
+      <md-scroll-view :scrolling-x="false" style="bottom: 90%;" :bouncing="false">
+        <md-field>
+          <md-cell-item v-for="item in 10" v-bind:key="item" title="ラーメン" no-border>
+            <img
+              class="holder"
+              slot="left"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRxiYsRILC5q59QRcTvXn9tAEDi3VCuXFtc1TLOQh5p-10gBm0J"
+            />
+            <p style="font-size: 0.7rem; margin-top: 5px;">￥100</p>
+            <md-stepper slot="right" v-model="value" min="0" max="5" />
+          </md-cell-item>
+        </md-field>
+      </md-scroll-view>
+      <!-- <div class="footer">
+        <md-button type="primary" round>Primary & Round</md-button>
+      </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-import {Field, CellItem, Switch} from 'mand-mobile'
+import { Field, CellItem, Switch } from "mand-mobile";
 
 export default {
-  name: 'cell-item-demo',
+  name: "cell-item-demo",
   components: {
     [Field.name]: Field,
     [CellItem.name]: CellItem,
-    [Switch.name]: Switch,
+    [Switch.name]: Switch
   },
   data() {
     return {
       open: false,
-    }
-  },
-}
-
+      current: 1,
+      items: [
+        { name: 1, label: "すべて" },
+        { name: 2, label: "主食" },
+        { name: 3, label: "ドリンク" },
+        { name: 4, label: "デザート" },
+        { name: 5, label: "新品" }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-.holder
-  display block
-  width 88px
-  height 88px
-  border-radius 44px
-  background-color #E6E6E6
+.body {
+  height: 100%;
+  background-color: #f9fafb;
+}
+
+.scroll {
+  box-sizing: border-box;
+  min-height: 100%;
+  height: 60vh;
+  padding-bottom: 100px;
+}
+
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 35vh;
+}
+
+.holder {
+  display: block;
+  width: 100px;
+  height: 100px;
+  border-radius: 20px;
+  background-color: #E6E6E6;
+}
+
+.header {
+  display: flex;
+  display: -webkit-flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f9fafb;
+}
+
+.title {
+  margin: 1rem 1rem 1rem 1rem;
+  font-size: 1.5rem;
+  border-radius: 1rem;
+}
 </style>
