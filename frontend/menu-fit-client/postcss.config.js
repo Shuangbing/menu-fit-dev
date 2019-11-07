a69fdc745c35df1789a7a1952f303c1d187f6ebe
Shuangbing <he.shuangbing@gmail.com>
2019-11-07 17:34:36 +0900

    remove old version client

--- frontend/menu-fit-client/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: [
-    require('postcss-pxtorem')({
-      rootValue: 30,
-      minPixelValue: 3,
-      propWhiteList: []
-    })
-  ]
-}
--- frontend/menu-fit-client/vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    outputDir: __dirname + '/../menu-fit-server/public/client/',
-    publicPath: '/'
-}
