e020f9019ca115957345ded0d1135a9553df6026
Shuangbing <he.shuangbing@gmail.com>
2019-12-29 14:55:50 +0900

    fix order view layout

--- frontend/client/components/OrderBottom.vue
+++ frontend/client/components/OrderBottom.vue
@@ -1,13 +1,13 @@
 <template>
-  <div>
-    <md-action-bar class="order-bottom" :actions="data" @click="openCart">
-      <md-icon name="setting" size="lg" @click="openSetting"></md-icon>
-      <span class="price">
-        <small>合計</small>
-        &yen;{{totalPrice}}
-      </span>
-    </md-action-bar>
-  </div>
+	<div>
+		<md-action-bar class="order-bottom" :actions="data" @click="openCart">
+			<md-icon name="setting" size="lg" @click="openSetting"></md-icon>
+			<span class="price">
+				<small>合計</small>
+				&yen;{{totalPrice}}
+			</span>
+		</md-action-bar>
+	</div>
 </template>
 
 <script lang="ts">
@@ -16,44 +16,44 @@ import { Toast } from "mand-mobile";
 
 @Component({})
 export default class OrderButton extends Vue {
-  @Prop() totalPrice: number;
+	@Prop() totalPrice: number;
 
-  data = [
-    {
-      type: "default",
-      icon: "edit",
-      text: "注文",
-      round: true
-    }
-  ];
+	data = [
+		{
+			type: "default",
+			icon: "edit",
+			text: "注文",
+			round: true
+		}
+	];
 
-  openSetting() {
-    this.$router.push("/user");
-  }
+	openSetting() {
+		this.$router.push("/user");
+	}
 
-  openCart() {
-    if (this.totalPrice > 0) {
-      this.$store.commit("setCartVisible", true);
-    } else {
-      Toast.info("料理を選んでから注文してください");
-    }
-  }
+	openCart() {
+		if (this.totalPrice > 0) {
+			this.$store.commit("setCartVisible", true);
+		} else {
+			Toast.info("料理を選んでから注文してください");
+		}
+	}
 }
 </script>
 
 <style>
-
 .order-bottom {
-  padding: 0.5rem 1.5rem 1.5rem 1.5rem;
+	padding-bottom: max(20px, constant(safe-area-inset-bottom));
+	padding-bottom: max(20px, env(safe-area-inset-bottom));
 }
 .price {
-  font-weight: 500;
-  font-size: 1.1rem;
-  color: #ff823a;
+	font-weight: 500;
+	font-size: 1.1rem;
+	color: #ff823a;
 }
 .price small {
-  margin-left: 10px;
-  font-size: 1rem;
-  color: #858b9c;
+	margin-left: 10px;
+	font-size: 1rem;
+	color: #858b9c;
 }
 </style>
--- frontend/client/pages/order/index.vue
+++ frontend/client/pages/order/index.vue
@@ -1,54 +1,57 @@
 <template>
-  <div>
-    <md-notice-bar v-show="!data.table_smoking" icon="warn" mode="closable" type="warning">禁煙席のため喫煙をご遠慮ください</md-notice-bar>
-    <OrderCart :menu="data.menu" :total="totalPrice()" :tableID="tableID" />
-    <md-dialog title="タイトル" :closable="true">内容</md-dialog>
-    <div class="header">
-      <div class="title">
-        <p>{{this.data.restrant_name}}</p>
-        <md-tag
-          size="large"
-          shape="circle"
-          type="fill"
-          fill-color="#6495ED"
-          font-color="#fff"
-        >{{this.data.table_no}}</md-tag>
-      </div>
-      <img class="title" src="/shop.jpg" width="100" height="100" />
-    </div>
-    <md-tab-bar :items="data.categories" @change="categoryFilter" :maxLength="10" />
-    <div style="height: 60vh; bottom: 0;">
-      <md-scroll-view
-        :scrolling-x="false"
-        :auto-reflow="true"
-        style="padding: 0 1rem;"
-      >
-        <md-cell-item
-          class="scroll-view-item"
-          v-for="item in menuCategories"
-          v-bind:key="item._id"
-          :title="item.title"
-          no-border
-        >
-          <img class="holder" slot="left" :src="item.picture" @click="showMenuDetail(item)" />
-          <p style="font-size: 0.7rem; margin-top: 5px;">￥{{item.price}}</p>
-          <a>
-            <md-tag
-              v-if="isHasAllergy(item.allergies) > 0"
-              size="small"
-              shape="fillet"
-              type="fill"
-              fill-color="rgba(255, 91, 96, .1)"
-              font-weight="normal"
-              font-color="#FF5B60"
-            >アレルギー</md-tag>
-          </a>
-          <md-stepper v-model="item.cart" slot="right" min="0" :max="item.max" read-only />
-        </md-cell-item>
-      </md-scroll-view>
+	<div class="order-view">
+		<md-notice-bar
+			v-show="!data.table_smoking"
+			icon="warn"
+			mode="closable"
+			type="warning"
+		>禁煙席のため喫煙をご遠慮ください</md-notice-bar>
+		<OrderCart :menu="data.menu" :total="totalPrice()" :tableID="tableID" />
+		<md-dialog title="タイトル" :closable="true">内容</md-dialog>
+		<div class="header">
+			<div class="title">
+				<p>{{this.data.restrant_name}}</p>
+				<md-tag
+					size="large"
+					shape="circle"
+					type="fill"
+					fill-color="#6495ED"
+					font-color="#fff"
+				>{{this.data.table_no}}</md-tag>
+			</div>
+			<img class="title" src="/shop.jpg" width="100" height="100" />
+		</div>
+		<div class="menu">
+      <md-tab-bar :items="data.categories" @change="categoryFilter" :maxLength="10" />
+			<md-scroll-view :scrolling-x="false" :auto-reflow="true" style="padding: 0 1rem;">
+				<md-cell-item
+					class="scroll-view-item"
+					v-for="item in menuCategories"
+					v-bind:key="item._id"
+					:title="item.title"
+					no-border
+				>
+					<img class="holder" slot="left" :src="item.picture" @click="showMenuDetail(item)" />
+					<p style="font-size: 0.7rem; margin-top: 5px;">￥{{item.price}}</p>
+					<a>
+						<md-tag
+							v-if="isHasAllergy(item.allergies) > 0"
+							size="small"
+							shape="fillet"
+							type="fill"
+							fill-color="rgba(255, 91, 96, .1)"
+							font-weight="normal"
+							font-color="#FF5B60"
+						>アレルギー</md-tag>
+					</a>
+					<md-stepper v-model="item.cart" slot="right" min="0" :max="item.max" read-only />
+				</md-cell-item>
+			</md-scroll-view>
+		</div>
+    <div class="footer">
       <OrderBottom :totalPrice="totalPrice()" />
     </div>
-  </div>
+	</div>
 </template>
 
 <script lang="ts">
@@ -58,113 +61,128 @@ import OrderCart from "../../components/OrderCart.vue";
 import { Dialog } from "mand-mobile";
 
 @Component({
-  components: {
-    OrderBottom,
-    OrderCart
-  }
+	components: {
+		OrderBottom,
+		OrderCart
+	}
 })
 export default class Order extends Vue {
-  data: any = {};
-  menuCategories = [];
+	data: any = {};
+	menuCategories = [];
 
-  isHasAllergy(allergies) {
-    const allergiesTemp = [];
-    allergies.forEach(element => {
-      if (this.data.user.allergies.indexOf(element) > -1) {
-        allergiesTemp.push(element);
-      }
-    });
-    return allergiesTemp.length;
-  }
+	isHasAllergy(allergies) {
+		const allergiesTemp = [];
+		allergies.forEach(element => {
+			if (this.data.user.allergies.indexOf(element) > -1) {
+				allergiesTemp.push(element);
+			}
+		});
+		return allergiesTemp.length;
+	}
 
-  showMenuDetail(menu) {
-    Dialog.alert({
-      title: menu.title,
-      content: menu.summary,
-      confirmText: "閉じる"
-    });
-  }
+	showMenuDetail(menu) {
+		Dialog.alert({
+			title: menu.title,
+			content: menu.summary,
+			confirmText: "閉じる"
+		});
+	}
 
-  async asyncData({ app, query }) {
-    if (query.tableId != undefined) localStorage.tableId = query.tableId;
-    let data = await app.$axios.$get("/client/order/" + localStorage.tableId);
-	data.categories.unshift({ name: "0", label: "すべて" });
-	console.log(data)
-    return {
-      tableID: localStorage.tableId,
-      data: data,
-      menuCategories: data.menu
-    };
-  }
+	async asyncData({ app, query }) {
+		if (query.tableId != undefined) localStorage.tableId = query.tableId;
+		let data = await app.$axios.$get(
+			"/client/order/" + localStorage.tableId
+		);
+		data.categories.unshift({ name: "0", label: "すべて" });
+		return {
+			tableID: localStorage.tableId,
+			data: data,
+			menuCategories: data.menu
+		};
+	}
 
-  totalPrice() {
-    var total = 0;
-    if (this.data.menu) {
-      this.data.menu.forEach(element => {
-        if (element.cart > 0) {
-          total = total + element.cart * element.price;
-        }
-      });
-      return total;
-    }
-  }
+	totalPrice() {
+		var total = 0;
+		if (this.data.menu) {
+			this.data.menu.forEach(element => {
+				if (element.cart > 0) {
+					total = total + element.cart * element.price;
+				}
+			});
+			return total;
+		}
+	}
 
-  async categoryFilter(item, index, prevIndex) {
-    this.menuCategories = [];
-    if (item.name == 0) {
-      this.menuCategories = this.data.menu;
-      return;
-    }
-    if (this.data.menu) {
-      this.data.menu.forEach(element => {
-        if (element.category == item.name) {
-          this.menuCategories.push(element);
-        }
-      });
-    }
-  }
+	async categoryFilter(item, index, prevIndex) {
+		this.menuCategories = [];
+		if (item.name == 0) {
+			this.menuCategories = this.data.menu;
+			return;
+		}
+		if (this.data.menu) {
+			this.data.menu.forEach(element => {
+				if (element.category == item.name) {
+					this.menuCategories.push(element);
+				}
+			});
+		}
+	}
 }
 </script>
 
 <style scoped>
-.body {
-  background-color: #f9fafb;
+body {
+	background-color: #f9fafb;
+}
+
+.order-view {
+	display: grid;
+	height: 100vh;
+  grid-template-rows: 20% 65% 15%;
+  grid-template-columns: 100%;
 }
 
 .scroll-view-item {
-  padding: 0.5rem 0;
-  font-size: 24px;
-  border-bottom: 0.5px solid #efefef;
+	padding: 0.5rem 0;
+	font-size: 24px;
+	border-bottom: 0.5px solid #efefef;
 }
 
 .footer {
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 35vh;
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	grid-row: 3;
+  grid-column: 1;
 }
 
 .holder {
-  display: block;
-  width: 100px;
-  height: 100px;
-  border-radius: 10px;
-  background-color: #e6e6e6;
+	display: block;
+	width: 4rem;
+	height: 4rem;
+	border-radius: 1rem;
+	background-color: #e6e6e6;
 }
 
 .header {
-  height: 20vh;
-  display: flex;
-  display: -webkit-flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #f9fafb;
+	grid-row: 1;
+  grid-column: 1;
+	display: flex;
+	display: -webkit-flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #f9fafb;
+}
+
+.menu {
+  grid-row: 2;
+  grid-column: 1;
 }
 
 .title {
-  margin: 1rem 1rem 1rem 1rem;
-  font-size: 1.2rem;
-  border-radius: 1rem;
+	margin: 1rem 1rem 1rem 1rem;
+	font-size: 1.2rem;
+	border-radius: 1rem;
 }
 </style>
